1154878faaa1b58083f29b0b090de0cf0c0c8d78
najmeh98 <foroughi.najmeh98@gmail.com>
2022-03-09 11:23:55 +0330

    update Media icon

--- components/Media.js
+++ components/Media.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Button } from "./Button/Button";
-import { EmpjiIcon, GifIcon, MediaIcon } from "./Icons/Icons";
+import { EmpjiIcon, GifIcon, MediaIcon, ShareICon } from "./Icons/Icons";
 import { FlexRow } from "./share/Containers";
 import { StandardButton } from "./StandardButton";
-
+import styled, { css } from "styled-components";
 export default function Media() {
   return (
-    <div style={{ display: "flex", alinItems: "center" }}>
-      <label htmlFor="file-input" style={{ cursor: "pointer" }}>
-        <Button icon>
-          <MediaIcon />
-        </Button>
-      </label>
-      <label htmlFor="file-input" style={{ cursor: "pointer" }}>
-        <Button icon>
-          <GifIcon />
-        </Button>
-      </label>
-      <label htmlFor="file-input" style={{ cursor: "pointer" }}>
-        <Button icon>
-          <EmpjiIcon />
-        </Button>
-      </label>
+    <div style={{ display: "flex", alingItems: "center" }}>
+      <Button icon>
+        <MediaIcon />
+      </Button>
+      <Button icon>
+        <GifIcon />
+      </Button>
+      <Button icon>
+        <EmpjiIcon />
+      </Button>
+      <ShareICon />
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  margin-top: 12px;
+  flex-direction: row;
+`;
