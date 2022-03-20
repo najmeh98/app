9ba51946ef128b32b872d3c2ed9e9d18b7e2452d
najmeh98 <foroughi.najmeh98@gmail.com>
2022-03-20 17:47:26 +0330

    update Header

--- components/Header.js
+++ components/Header.js
@@ -5,11 +5,9 @@ import Space from "./share/Space";
 
 export default function Header({ children }) {
   return (
-    <Wrapper
-      style={{ height: "55px", backgroundColor: " hsla(0,0%,100%,0.8)" }}
-    >
-      {children}
-    </Wrapper>
+    <Row>
+      <Wrapper>{children}</Wrapper>
+    </Row>
   );
 }
 
@@ -17,10 +15,21 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  //width: 100%;
-  height: 53px;
+  /* height: 55px; */
   border-bottom: 1px solid rgb(230, 236, 240);
-  // position: fixed;
   padding-left: 15px;
   padding-right: 15px;
+  background-color: rgba(255, 255, 255, 0.85);
+  backdrop-filter: blur(12px);
+  /* width: 100%; */
+`;
+
+export const Row = styled.div`
+  position: sticky;
+  width: 100%;
+  right: 0px;
+  top: 0px;
+  left: 0;
+  z-index: 3;
+  /* height: 100%; */
 `;
